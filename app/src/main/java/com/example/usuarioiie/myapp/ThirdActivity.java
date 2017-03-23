ad5eee68c2d430252f9264c37ec83dacb98b5b1f
devorosales <orosales.avila@gmail.com>
2017-03-23 16:52:42 -0600

    Cambios donde agregue los permisos de llamada

--- app/src/main/java/com/example/usuarioiie/myapp/ThirdActivity.java
+++ app/src/main/java/com/example/usuarioiie/myapp/ThirdActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -42,7 +43,7 @@ public class ThirdActivity extends AppCompatActivity {
                     //Comprobar version actual de android que estamos corriendo
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
-                    }else{
+                    } else {
                         olderVersion(phoneNumber);
                     }
                 }
@@ -51,22 +52,42 @@ public class ThirdActivity extends AppCompatActivity {
             private void olderVersion(String phoneNumber) {
                 Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
                 if (CheckPermission(Manifest.permission.CALL_PHONE)) {
-                    startActivity(intentCall)
+                    startActivity(intentCall);
                 } else {
                     Toast.makeText(ThirdActivity.this, "You decline the access", Toast.LENGTH_LONG).show();
                 }
-
-                ;
             }
-
-
         });
-
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // estamos en el caso del telefono
+        switch (requestCode) {
+            case PHONE_CALL_CODE:
+                String permision = permissions[0];
+                int resul = grantResults[0];
+
+                if (permision.equals(Manifest.permission.CALL_PHONE)) {
+                    //COMPROBAR SI HA SIDO ACEPTADO O DENEGADO LA OPCION DE PERMISO
+                    if (resul == PackageManager.PERMISSION_GRANTED) {
+                        //Concedio su permiso
+                        String phoneNumber = editTextPhone.getText().toString();
+                        Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)return;
+                        startActivity(intentCall);
+                    }
+                    else {
+                        //No concedio su permiso
+                        Toast.makeText(ThirdActivity.this, "You decline the access", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 
     private boolean CheckPermission(String permission) {
