e34b9c1c343239d8a4eefb8f2adab95e64b2840e
devorosales <orosales.avila@gmail.com>
2017-04-18 12:54:55 -0500

    Cambios donde agregue los permisos de llamada

--- app/src/main/java/com/example/usuarioiie/myapp/ThirdActivity.java
+++ app/src/main/java/com/example/usuarioiie/myapp/ThirdActivity.java
@@ -42,7 +42,31 @@ public class ThirdActivity extends AppCompatActivity {
                 if (phoneNumber != null && !phoneNumber.isEmpty()) {
                     //Comprobar version actual de android que estamos corriendo
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+
+                        //Comprobar si ha aceptado, si nunca a aceptado o nunca se le ha preguntado
+                        if(CheckPermission(Manifest.permission.CALL_PHONE)){
+                            //Ha aceptado
+                            Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+phoneNumber));
+                            if (ActivityCompat.checkSelfPermission(ThirdActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)return;
+                            startActivity(i);
+                        }else{
+                            //o no a aceptado o es la primera vez que se le pregunta
+                            if(!shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)){
+                                // No se le ha preguntado aun
+                                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+                            }else{
+                                //Ha denegado
+                                Toast.makeText(ThirdActivity.this, "Please enabled the request permission", Toast.LENGTH_LONG).show();
+                                Intent ver = new Intent();
+                                ver.addCategory(Intent.CATEGORY_DEFAULT);
+                                ver.setData(Uri.parse("package:"+getPackageName()));
+                                ver.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                ver.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                ver.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                                startActivity(ver);
+                            }
+                        }
+
                     } else {
                         olderVersion(phoneNumber);
                     }
@@ -58,6 +82,20 @@ public class ThirdActivity extends AppCompatActivity {
                     startActivity(intentCall);
                 } else {
                     Toast.makeText(ThirdActivity.this, "You decline the access", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        //Boton para la direccion WEB
+        imgButtonWeb.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String url = editTextWeb.getText().toString();
+                if(url != null && !url.isEmpty()){
+                    Intent intentWeb = new Intent();
+                    intentWeb.setAction(Intent.ACTION_VIEW);
+                    intentWeb.setData(Uri.parse("http://"+url));
+                    startActivity(intentWeb);
                 }
             }
         });
